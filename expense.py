cfc5333a4f287a0d7c45634d5cacb020516b7f25
Thomas WITZ <86101452+SchauT@users.noreply.github.com>
2023-09-20 21:55:30 +0200

    init

--- expense.py
+++ expense.py
@@ -1,4 +1,8 @@
 from PyInquirer import prompt
+import csv
+from user import get_users
+
+users = []
 
 expense_questions = [
     {
@@ -12,19 +16,59 @@ expense_questions = [
         "message":"New Expense - Label: ",
     },
     {
-        "type":"input",
+        "type":"list",
         "name":"spender",
         "message":"New Expense - Spender: ",
+        "choices": users
     },
 
 ]
 
-
+expense_questions_shared = [
+    {
+        "type":"checkbox",
+        "name":"shared",
+        "message":"New Expense - Shared: ",
+        "choices": users
+    },
+]
 
 def new_expense(*args):
+    users = get_users()
+    print(users)
+
     infos = prompt(expense_questions)
-    # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
+
+    print(infos)
+
+    for user in users:
+        if user['name'] == infos['spender']:
+            user['checked'] = True
+
+    print(users)
+
+    sharing_infos = prompt(expense_questions_shared)
+
+    infos_list = list(infos.values())
+    sharing_infos_list = list(sharing_infos.values())
+    with open('expense_report.csv', 'a', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows([infos_list + sharing_infos_list])
+
+    # with open('users.csv', 'w', newline='') as f:
+    #     writer = csv.writer(f)
+    #     for user in users:
+    #         if sharing_infos_list.contains(user['name']):
+    #             user['debt'] = int(user['debt']) + int(infos['amount']) / len(sharing_infos_list)
+    #         writer.writerows([[user['name'], user['debt']]])
+
     print("Expense Added !")
     return True
 
+def get_status():
+    users = get_users()
 
+    for user in users:
+        print("- " + user['name'] + " : " + str(user['debt']))
+
+    return
--- user.py
+++ user.py
@@ -1,8 +1,42 @@
-from PyInquirer import prompt
-user_questions = [
+from PyInquirer import prompt, print_json
+import csv
 
+user_questions = [
+    {
+        "type":"input",
+        "name":"name",
+        "message":"New User - Name: ",
+    },
 ]
 
 def add_user():
     # This function should create a new user, asking for its name
+    infos = prompt(user_questions)
+
+    # Reading if user already exists
+    with open('users.csv', 'r', newline='') as f:
+        # read lines and check if user already exists
+        reader = csv.reader(f)
+        for row in reader:
+            if row[0] == infos['name']:
+                print("User already exists !")
+                return
+
+    # Writing the informations on external file
+    with open('users.csv', 'a', newline='') as f:
+        writer = csv.writer(f)
+        infos['debt'] = 0
+        writer.writerows([infos.values()])
+
+    print(infos['name'] + " added !")
     return
+
+def get_users():
+    users = []
+
+    with open('users.csv', 'r', newline='') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            users.append({"name": row[0], "checked": False}) #, "debt": row[1]})
+
+    return users
